893510b370913e13eae81b169e0987dc0f1009f2
Kevin <kevin@Kevins-MacBook-Pro.local>
2022-02-22 19:53:28 +0530

    formula

--- socios.rb
+++ socios.rb
@@ -4,8 +4,8 @@
 class Socios < Formula
   desc "development formula - Socios"
   homepage "https://github.com/Bsivakumar29/socios"
-  url "https://github.com/Bsivakumar29/socios/archive/refs/tags/v1.1.2.tar.gz"
-  sha256 "d982d7a95680bac90a07cddff839c9a23dc5cce4a88279964310c4f1f022eb04"
+  url "https://github.com/Bsivakumar29/socios/archive/refs/tags/v1.1.3.tar.gz"
+  sha256 "b7bf9adae2280faec9f84fbfbc3394e0a48d5902d14ca9c6b34ac92422067aeb"
   license ""
 
   def install
