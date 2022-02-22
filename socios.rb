2c1f61cebf33d17c5ba1534a071d633161f67639
Kevin <kevin@Kevins-MacBook-Pro.local>
2022-02-22 21:20:01 +0530

    vbox commit

--- socios.rb
+++ socios.rb
@@ -1,11 +1,10 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Socios < Formula
   desc "development formula - Socios"
   homepage "https://github.com/Bsivakumar29/socios"
-  url "https://github.com/Bsivakumar29/socios/archive/refs/tags/v1.1.3.tar.gz"
-  sha256 "b7bf9adae2280faec9f84fbfbc3394e0a48d5902d14ca9c6b34ac92422067aeb"
+  url "https://github.com/Bsivakumar29/socios/archive/refs/tags/v1.1.4.tar.gz"
+  sha256 "f7ae94c82098f06920b0cf9c193ee28bd02119b14902bb2378ccfc9b50fc7acd"
   license ""
 
   def install
