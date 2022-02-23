bd13bbac864905b093df40e6e50aef5c3fe0839e
bsivakumar29 <kevin@Kevins-MacBook-Pro.local>
2022-02-23 22:30:18 +0530

    image added

--- socios.rb
+++ socios.rb
@@ -1,10 +1,11 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Socios < Formula
   desc "development formula - Socios"
   homepage "https://github.com/Bsivakumar29/socios"
-  url "https://github.com/Bsivakumar29/socios/archive/refs/tags/v1.1.4.tar.gz"
-  sha256 "f7ae94c82098f06920b0cf9c193ee28bd02119b14902bb2378ccfc9b50fc7acd"
+  url "https://github.com/Bsivakumar29/socios/archive/refs/tags/v1.2.0.tar.gz"
+  sha256 "3c0973cd259c0a64690520c5d450a64ed073a414581fb374f619409fa5b4f32b"
   license ""
 
   def install
