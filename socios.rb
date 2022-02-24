5eb8e6f7e45e10c2b718da3a95859da0afab5627
bsivakumar29 <kevin@Kevins-MacBook-Pro.local>
2022-02-24 21:43:25 +0530

    help menu

--- socios.rb
+++ socios.rb
@@ -3,9 +3,9 @@
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Socios < Formula
   desc "development formula - Socios"
-  homepage "https://github.com/Bsivakumar29/socios"
-  url "https://github.com/Bsivakumar29/socios/archive/refs/tags/v1.2.0.tar.gz"
-  sha256 "3c0973cd259c0a64690520c5d450a64ed073a414581fb374f619409fa5b4f32b"
+  homepage "https://github.com/Bsivakumar29/socios.git"
+  url "https://github.com/Bsivakumar29/socios/archive/refs/tags/v1.2.1.tar.gz"
+  sha256 "0b9479f47fe9f65ecce6009fbaadcb5ee72c23997218bb7f6001cb10ad5b6b44"
   license ""
 
   def install
