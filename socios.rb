9184e9d7f793f9b0377ad79db9295c68518f1a47
bsivakumar29 <kevin@Kevins-MacBook-Pro.local>
2022-02-25 12:54:24 +0530

    version added

--- socios.rb
+++ socios.rb
@@ -2,10 +2,10 @@
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Socios < Formula
-  desc "development formula - Socios"
+  desc "development formula -Socios"
   homepage "https://github.com/Bsivakumar29/socios"
-  url "https://github.com/Bsivakumar29/socios/archive/refs/tags/v1.2.2.tar.gz"
-  sha256 "854566bb44074512a3f8cdbcf5c4506a2e6bc53dadc728ad9e85c28bdf2a2c27"
+  url "https://github.com/Bsivakumar29/socios/archive/refs/tags/v1.2.3.tar.gz"
+  sha256 "e48c21a70b3c8fa2cf9e17786475272668c6d17b7c8e41b59f6d233c6be6e488"
   license ""
 
   def install
